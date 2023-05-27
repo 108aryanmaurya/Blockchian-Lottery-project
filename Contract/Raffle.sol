130bf828e51b52f97ff53b6b885ac365a3534fc5
108aryanmaurya <108aryanmaurya@gmail.com>
2023-05-27 11:01:03 +0530

    first commit

---
+++ Contract/Raffle.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+error Raffle__NotEnoughEthEntered();
+
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 entrancefee) {
+        i_entranceFee = entrancefee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthEntered();
+        }
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayers(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
